5b9e56f7bb96aac6ce16c750a2fd19a939cec4f8
Edú Isaí <Nitro 5@DESKTOP-JU8O3RS>
2022-09-16 14:03:49 -0500

    Transport ADT

---
+++ TransportADT/Register.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportADT
+{
+    internal class Register
+    {
+        static void Main(string[] args)
+        {
+            Person P1 = new Person();
+            Vehicle V1 = new Vehicle();
+            License L1 = new License();
+
+            P1.Name = "Juan";
+            P1.SurName = "Vasquez";
+            P1.ID = 1234;
+            P1.age = 19;
+            P1.gender = "M";
+
+            V1.Vtype = "SUV";
+            V1.description = "bLABLA";
+            V1.brand = "Ford";
+            V1.year = 2019;
+            V1.color = "Red";
+
+            L1.Type = "Car";
+
+
+        }
+    }
+}
---
+++ TransportADT/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportADT
+{
+    internal class Vehicle
+    {
+     public string Vtype { get; set; }
+        public int year { get; set; }
+        public string brand { get; set; }
+        public string description   { get; set; }
+        public string wheels { get; set; }
+        public string color { get; set; }
+
+    }
+}
